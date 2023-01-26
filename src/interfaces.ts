e50c86c3a65878171e37d8554ae3955738176176
gartkod <goran@artkod.com>
2023-01-26 13:34:52 +0100

    Izmjene

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,21 +1,30 @@
-export interface UserType {
+export interface UsersInterface { /* kao što vidiš za sve si stavio "export interface" onda je naming konvencija Interface, Postoji i Type ali se odnosi na tipove podataka ne na interface */
     name: string; surname: string; age: number
 }
-  
-export interface UsersType extends Array<UserType>{}
-
-export interface NameAgeType {
+export interface NameAgeInterface { 
     name: string; age: number
 }
 
-export interface NameSurnameType {
+export interface NameSurnameInterface {
     name: string; surname: string
 }
 
-export interface SurnameAgeType {
+export interface SurnameAgeInterface {
     surname: string; age: number;
 }
 
-export interface SurnameAgeTypeProps {
-    children?: SurnameAgeType;
+export interface SurnameAgeInterfaceProps {
+    children?: SurnameAgeInterface;
+}
+
+
+
+
+
+// sve je ovo moglo biti
+export interface UserInterface {
+    name?: string;
+    surname?: string;
+    age?: number;
+    children?: any
 }
